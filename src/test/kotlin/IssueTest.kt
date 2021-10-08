e611f60e9fe4c0cc431efc285d5d8525d3d0165a
Simon Dean <simon@simondean.org>
2021-10-08 13:26:32 +0100

    Add Mockito example

--- src/test/kotlin/IssueTest.kt
+++ src/test/kotlin/IssueTest.kt
@@ -1,12 +1,21 @@
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito.verify
 
 class IssueTest {
 
     @Test
-    fun `will silently be skipped`() = runBlocking {
+    fun `will be silently skipped`() = runBlocking {
         launch { doSomething() }
+    }
+
+    @Test
+    fun `also will be silently skipped`() = runBlocking {
+        // This bug is not specific to Mockito but the bug is easily triggered by putting a verify() call at the end of a unit test
+        val value = "anything"
+
+        verify(value).toString()
     }
 
     @Test
